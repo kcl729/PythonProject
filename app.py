c0ce5e2aee2c6c58cf1ecadf29a0e5b7b304288b
amber s <asillis1@users.noreply.github.com>
2021-04-22 13:44:04 -0400

    add docstrings

--- app.py
+++ app.py
@@ -18,11 +18,13 @@ questions_count = 0
 # startpage: 
 @app.route('/')
 def get_started():
+    """returns index page"""
     return render_template('index.html')
 
 # startpage data processing
 @app.route('/', methods=["GET", "POST"])
 def questions_requested():
+    """processes user's desired number of questions and returns welcome page"""
     global total_questions
     if request.method == "POST":
         count = request.form.get('count')
@@ -32,6 +34,8 @@ def questions_requested():
 # actual app:
 @app.route('/questions')
 def show_problem():
+    """displays porblem on problem page during session, ends session when user answered desired amount of questions
+    returns questions or end page"""
     global questions_count
     global correct_answers_count
     if questions_count < total_questions:
@@ -46,11 +50,13 @@ def show_problem():
 # check answers:
 @app.route('/questions', methods=["GET", "POST"])  
 def reply():
+    """checks user's answers and returns checkanswer page with reply
+    and troubeshoots error if user does not put anything before clicking submit"""
     try:
         if request.method == "POST":
             answer = float(request.form['math_answer'])
             reply = check_answer(answer)     
-        return render_template('checkanswer.html', reply=reply, questions_count=questions_count, correct_answers_count=correct_answers_count)
+        return render_template('checkanswer.html', reply=reply, questions_count=questions_count)
     except ValueError:
         return render_template("error.html")  
         
--- problems.py
+++ problems.py
@@ -5,16 +5,21 @@ global correct_answers_count
 correct_answers_count = 0
 
 def generate_problem():
+    """generate and return a random problem statement from the problem/answer dictionary"""
     global random_problem_key
     random_problem_key = random.choice(list(problem_dictionary.keys()))
     problem_statement =  problem_dictionary[random_problem_key][0]   
     return problem_statement 
 
 def reset_count(correct_answers_count):
+    """ reset the correct_answers_count after a session has ended so that user may do another session
+    returns: None -- function just edits the global variable""" 
     correct_answers_count = 0
     return None
 
 def check_answer(answer):
+    """ check user's answer against the correct answer in the dictionary 
+    returns: a string that explains whether or not the answer was right and what the correct answer would be.  """
     solution = problem_dictionary[random_problem_key][1]
     # global questions_count
     # questions_count += 1
@@ -28,6 +33,8 @@ def check_answer(answer):
     return reply
 
 def show_results():
+    """function that displays the user's results at the end of a practice session & resets it to 0 in case the user wants to start another session
+    returns string with message to share user statistics"""
     global correct_answers_count
     rightanswers = correct_answers_count
     results = f'You got a total of {rightanswers} questions correct.'
@@ -35,6 +42,8 @@ def show_results():
     return results 
 
 def q_req(count):
+    """function that processes the index form that indicates how many questions user want to do in one session
+    returns integer"""
     if count == "five":
         total_questions = 5
     elif count == "ten":
